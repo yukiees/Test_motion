996df43a030a116d97cd9fdcfa8dec054da32576
yukiees <yukiii8989@gmail.com>
2020-11-22 19:21:41 +0900

    Initial Commit

---
+++ Test_motion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_motion
+//
+//  Created by 松島優希 on 2020/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
