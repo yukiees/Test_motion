ca4565762162d42e8761b6681674a886a79f5d8f
yukiees <yukiii8989@gmail.com>
2020-11-23 02:34:26 +0900

    datePickerView Comp

--- Test_motion/ViewController.swift
+++ Test_motion/ViewController.swift
@@ -5,15 +5,64 @@
 //  Created by 松島優希 on 2020/11/22.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var dateField: UITextField!
 
+        let datePicker = UIDatePicker()
 
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            createDatePicker()
+        }
+
+       func createDatePicker(){
+
+            // DatePickerModeをDate(日付)に設定
+            datePicker.datePickerMode = .date
+
+            // DatePickerを日本語化
+            datePicker.locale = NSLocale(localeIdentifier: "ja_JP") as Locale
+
+            // textFieldのinputViewにdatepickerを設定
+            dateField.inputView = datePicker
+            
+            // モーションをくるくるに設定
+            datePicker.preferredDatePickerStyle = .wheels
+
+            // UIToolbarを設定
+            let toolbar = UIToolbar()
+            toolbar.sizeToFit()
+
+            // Doneボタンを設定(押下時doneClickedが起動)
+            let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneClicked))
+
+            // Doneボタンを追加
+            toolbar.setItems([doneButton], animated: true)
+
+            // FieldにToolbarを追加
+            dateField.inputAccessoryView = toolbar
+        }
+
+        @objc func doneClicked(){
+            let dateFormatter = DateFormatter()
+
+            // 持ってくるデータのフォーマットを設定
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .none
+            dateFormatter.locale    = NSLocale(localeIdentifier: "ja_JP") as Locale?
+            dateFormatter.dateStyle = DateFormatter.Style.medium
+            dateFormatter.dateFormat  = "yyyy年MM月dd日";
+            
+            // textFieldに選択した日付を代入
+            dateField.text = dateFormatter.string(from: datePicker.date)
+
+            // キーボードを閉じる
+            self.view.endEditing(true)
+        }
 }
 
